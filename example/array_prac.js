31152ce764404529544a104d8a3b93a86726efb4
joohylee <joohylee@student.42seoul.kr>
2020-09-22 11:06:23 +0900

    array_prac.js

---
+++ example/array_prac.js
@@ -0,0 +1,11 @@
+var car1 = "Ssab";
+var car2 = "Volvo";
+var car3 = "BMW";
+
+var cars = ["Ssab", "Volvo", "BMW", 23, 23.113];
+//int[] odds = {1, 3, 5, 7, 9, "Ssab"};
+
+console.log(cars);
+console.log(cars[0]);
+console.log(cars[1]);
+console.log(cars[2]);
