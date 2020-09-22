9e291c8f5f125a6e6e2e7c53c112ae8e76be0844
joohylee <joohylee@student.42seoul.kr>
2020-09-22 17:56:01 +0900

    request 완성

--- http_server.js
+++ http_server.js
@@ -1,9 +1,17 @@
 var http = require("http");
+const request = require("request");
+var parseString = require("xml2js").parseString;
 
 http
   .createServer(function (req, res) {
-    var body = "hello Server";
-    res.setHeader("Content-Type", "text/plain; charset=utf-8");
-    res.end("안녕하세요");
+	var body = "<html><h1>test</h1></html>";
+	console.log("요청이 들어왔습니다.");
+	request("https://naver.com", function (error, response, body) {
+      res.setHeader("Content-Type", "text/html; charset=utf-8");
+      res.end(body);
+    });
   })
+
   .listen(3000);
+
+  // http://localhost:3000 연결
