57263ac9f629f8a6e0e6c5d5dd630104bf91071d
joohylee <joohylee@student.42seoul.kr>
2020-09-22 17:22:25 +0900

    request 모듈 추가

---
+++ example/http_server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+http
+  .createServer(function (req, res) {
+	var body = "<html><h1>test</h1></html>";
+	console.log("요청이 들어왔습니다.");
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end(body);
+  })
+  .listen(3000);
+
+// http://localhost:3000/
+// 172.168.0.1:3000 (nodejs http server)
+// 127.0.0.1:3306 (MySQL)
