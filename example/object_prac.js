460044ed90ab2572228cd98d79f831d085e5a4ee
joohylee <joohylee@student.42seoul.kr>
2020-09-22 10:51:19 +0900

    object_prac.js

---
+++ example/object_prac.js
@@ -0,0 +1,23 @@
+var car = {
+	name: "sonata",
+	ph: "500ph",
+	start: function() {
+		console.log("engine is starting");
+	},
+	stop: function() {
+		console.log("engine is stopped");
+	},
+};
+
+var car2 = {
+	name: "bmw",
+	ph: "380ph",
+	start: function() {
+		console.log("engine is starting");
+	},
+	stop: function() {
+		console.log("engine is stopped");
+	},
+};
+
+console.log(car);
