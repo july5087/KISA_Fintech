fe3b83b2c2bf5e746851699349369d36b4133b86
joohylee <joohylee@student.42seoul.kr>
2020-09-23 10:23:45 +0900

    EJS 사용설정 완료

--- express._server.js
+++ express._server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+app.set("views", __dirname + "/views"); //ejs를 사용하기 위한 디렉토리 설정
+app.set("view engine", "ejs"); //ejs를 사용하기 위한 뷰 엔진 설정
+
 app.get("/", function (req, res) {
   res.send("Hello World");
 });
